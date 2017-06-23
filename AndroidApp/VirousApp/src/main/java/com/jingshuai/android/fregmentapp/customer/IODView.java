3f3d52eaeb172fea7ca8f984c47f060fc3a2a063
jingshuai.zhang <jingshuai.zhang@ericsson.com>
2017-06-23 16:39:40 +0800

    Add IODView update

--- AndroidApp/VirousApp/src/main/java/com/jingshuai/android/fregmentapp/customer/IODView.java
+++ AndroidApp/VirousApp/src/main/java/com/jingshuai/android/fregmentapp/customer/IODView.java
@@ -122,9 +122,10 @@ public class IODView extends View {
         super.onDraw(canvas);
         mPaint = new Paint();
         mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setPathEffect(new CornerPathEffect(80));
+        //mPaint.setPathEffect(new CornerPathEffect(80));
         mPaint.setStrokeWidth(4.0f);
-        mPaint.setColor(Color.GREEN);
+        //mPaint.setColor(Color.GREEN);
+        mPaint.setColor(getResources().getColor(R.color.lightskyblue));
         //canvas.drawCircle(300.0f,400.0f,80.0f,mPaint);
         //canvas.drawCircle(300.0f,400.0f,150.0f,mPaint);
 
@@ -132,14 +133,16 @@ public class IODView extends View {
 
         int bigRectX = 150;
         int bigRectY = 50;
-        canvas.drawRect(bigRectX, bigRectY, width-150,targetY, mPaint);
+        //canvas.drawRect(bigRectX, bigRectY, width-150,targetY, mPaint);
+        drawBigRect(bigRectX,bigRectY,width-150,targetY,50,mPaint, canvas);
 
         drawFocusRect(circleX,circleY,mPaint,canvas);
         //canvas.drawRect(circleX-50, circleY-50, circleX+50,circleY+50, mPaint);
 
         mPaint.setStrokeWidth(5.0f);
-        mPaint.setColor(Color.GREEN);
-        mPaint.setPathEffect(new DashPathEffect(new float[] {20, 10}, 1));
+       // mPaint.setColor(Color.GREEN);
+        mPaint.setColor(getResources().getColor(R.color.lightskyblue));
+        //mPaint.setPathEffect(new DashPathEffect(new float[] {20, 10}, 1));
         Path mPath = new Path();
         //
         int line1X = circleX-50;
@@ -166,6 +169,63 @@ public class IODView extends View {
 
     }
 
+    private void drawBigRect(int x,int y,int tox,int toy,int sideLength, Paint mPaint, Canvas canvas){
+        //        0 -------- 1
+        //
+        //        3 -------- 2
+        int SIDE_LENGTH = sideLength;
+        Paint mPaint1 = new Paint();
+        mPaint1.setStyle(Paint.Style.STROKE);
+        //mPaint1.setPathEffect(new DashPathEffect(new float[] {20, 10}, 1));
+        mPaint1.setStrokeWidth(4.0f);
+        mPaint1.setColor(getResources().getColor(R.color.lightskyblue));
+
+        Point point0 = new Point(x,y);
+        Point point1 = new Point(tox,y);
+        Point point2 = new Point(tox,toy);
+        Point point3 = new Point(x,toy);
+
+        //draw point 0 lines
+        Point pointStart = new Point(point0.x,point0.y+SIDE_LENGTH);
+        Point pointEnd = new Point(point0.x+SIDE_LENGTH,point0.y);
+        Path mPath = new Path();
+        mPath.moveTo(pointStart.x, pointStart.y);
+        mPath.lineTo(point0.x,point0.y);
+        mPath.lineTo(pointEnd.x,pointEnd.y);
+
+
+        //draw point 1 lines
+        Point pointStart1 = new Point(point1.x-SIDE_LENGTH,point1.y);
+        Point pointEnd1 = new Point(point1.x,point1.y+SIDE_LENGTH);
+
+        mPath.moveTo(pointStart1.x, pointStart1.y);
+        mPath.lineTo(point1.x,point1.y);
+        mPath.lineTo(pointEnd1.x,pointEnd1.y);
+
+
+        //draw point 2 lines
+        Point pointStart2 = new Point(point2.x-SIDE_LENGTH,point2.y);
+        Point pointEnd2 = new Point(point2.x,point2.y-SIDE_LENGTH);
+
+        mPath.moveTo(pointStart2.x, pointStart2.y);
+        mPath.lineTo(point2.x,point2.y);
+        mPath.lineTo(pointEnd2.x,pointEnd2.y);
+
+
+
+        //draw point 3 lines
+        Point pointStart3 = new Point(point3.x,point3.y-SIDE_LENGTH);
+        Point pointEnd3 = new Point(point3.x+SIDE_LENGTH,point3.y);
+
+        mPath.moveTo(pointStart3.x, pointStart3.y);
+        mPath.lineTo(point3.x,point3.y);
+        mPath.lineTo(pointEnd3.x,pointEnd3.y);
+        canvas.drawPath(mPath,mPaint1);
+
+
+    }
+
+
     private void drawFocusRect(int x,int y,Paint mPaint, Canvas canvas){
         //        0 -------- 1
         //
@@ -175,7 +235,7 @@ public class IODView extends View {
         mPaint1.setStyle(Paint.Style.STROKE);
         //mPaint1.setPathEffect(new DashPathEffect(new float[] {20, 10}, 1));
         mPaint1.setStrokeWidth(4.0f);
-        mPaint1.setColor(Color.BLUE);
+        mPaint1.setColor(getResources().getColor(R.color.lightskyblue));
 
         Point point0 = new Point(x-focusRectLength/2,y-focusRectLength/2);
         Point point1 = new Point(x+focusRectLength/2,y-focusRectLength/2);
