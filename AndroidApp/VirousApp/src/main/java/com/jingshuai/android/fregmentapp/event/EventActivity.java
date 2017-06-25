f236648d8849d7b5c09f622391a62c4c296fe91c
Jingshuai <jingshuaizh@163.com>
2017-06-25 23:01:34 +0800

    [androidapps]add event activity 2

--- AndroidApp/VirousApp/src/main/java/com/jingshuai/android/fregmentapp/event/EventActivity.java
+++ AndroidApp/VirousApp/src/main/java/com/jingshuai/android/fregmentapp/event/EventActivity.java
@@ -1,15 +1,31 @@
 package com.jingshuai.android.fregmentapp.event;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.jingshuai.android.fregmentapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class EventActivity extends AppCompatActivity {
+
+    @BindView( R.id.imageView )
+    public ImageButton imageButton ;
+    @BindView( R.id.constraintLayout )
+    public ConstraintLayout constraintLayout ;
+    @BindView( R.id.linearLayout )
+    public LinearLayout linearLayout ;
+    @BindView( R.id.relativeLayout )
+    public RelativeLayout relativeLayout ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +33,7 @@ public class EventActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ButterKnife.bind(this);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
