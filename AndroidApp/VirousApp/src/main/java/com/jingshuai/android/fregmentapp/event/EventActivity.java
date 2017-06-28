6758f57ee59f6701f47baae4c89bf0e1e61e336b
jingshuai.zhang <jingshuai.zhang@ericsson.com>
2017-06-28 17:38:10 +0800

    [AndroidApp]update event activity

--- AndroidApp/VirousApp/src/main/java/com/jingshuai/android/fregmentapp/event/EventActivity.java
+++ AndroidApp/VirousApp/src/main/java/com/jingshuai/android/fregmentapp/event/EventActivity.java
@@ -1,13 +1,14 @@
 package com.jingshuai.android.fregmentapp.event;
 
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
@@ -19,9 +20,9 @@ import butterknife.ButterKnife;
 public class EventActivity extends AppCompatActivity {
 
     @BindView( R.id.imageView )
-    public ImageButton imageButton ;
+    public ImageView imageView ;
     @BindView( R.id.constraintLayout )
-    public ConstraintLayout constraintLayout ;
+    public LinearLayout constraintLayout ;
     @BindView( R.id.linearLayout )
     public LinearLayout linearLayout ;
     @BindView( R.id.relativeLayout )
